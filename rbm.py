a4f7e5f72ac69d10413a74e808fbb9946cebb49f
ApocalyVec <zli12@wpi.edu>
2020-05-23 05:39:06 -0400

    complete testing rbm

--- rbm.py
+++ rbm.py
@@ -9,16 +9,29 @@ import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
 
-all_movie = pd.read_csv('ml-1m/movies.dat', sep='::', header=None, engine='python', encoding='latin-1')
-all_users = pd.read_csv('ml-1m/users.dat', sep='::', header=None, engine='python', encoding='latin-1')
-all_ratings = pd.read_csv('ml-1m/ratings.dat', sep='::', header=None, engine='python', encoding='latin-1')
 
-# prepare the training set and the test set
-training_set = pd.read_csv('ml-100k/u1.base', delimiter='\t')
-training_set = np.array(training_set, dtype='int')
-# prepare the test set and the test set
-test_set = pd.read_csv('ml-100k/u1.test', delimiter='\t')
-test_set = np.array(test_set, dtype='int')
+class RBM:
+    def __init__(self, nv, nh):
+        self.W = torch.randn(nh, nv)
+        self.a = torch.randn(1, nh)  # bias for the hidden nodes, one addition dimension for batch
+        self.b = torch.randn(1, nv)  # bias for the visible nodes
+
+    def sample_h(self, x):  # sample the activation of the hidden nodes given the value of the visible nodes
+        wx = torch.mm(x, self.W.t())
+        activation = wx + self.a.expand_as(wx)  # expand the bias according to batch, add the bias,
+        p_h_given_v = torch.sigmoid(activation)  # probability of the hidden nodes are activated given the visible nodes
+        return p_h_given_v, torch.bernoulli(p_h_given_v)
+
+    def sample_v(self, y):  # sample the value of the visible nodes given values of the hidden nodes
+        wy = torch.mm(y, self.W)
+        activation = wy + self.b.expand_as(wy)
+        p_v_given_h = torch.sigmoid(activation)
+        return p_v_given_h, torch.bernoulli(p_v_given_h)
+
+    def train(self, v0, vk, ph0, phk):  # using contrasted divergence
+        self.W += (torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)).t()
+        self.b += torch.sum((v0 - vk), 0)  # keeping b to be a 2D tensor
+        self.a += torch.sum((ph0 - phk), 0)
 
 
 # Getting the number of users and movies
@@ -53,5 +66,70 @@ def convert_bm_matrix(data, nb_users, nb_movies):
     return new_data
 
 
+def convert_to_binary(data: torch.FloatTensor, threshold=3):
+    rtn = data.clone()
+    rtn[data == 0.] = -1.
+    rtn[[x & y for (x, y) in zip([data > 0.], [data < threshold])]] = 0.
+    rtn[data >= threshold] = 1.
+    return rtn
+
+
+# Script body ##########################################################################################
+all_movie = pd.read_csv('ml-1m/movies.dat', sep='::', header=None, engine='python', encoding='latin-1')
+all_users = pd.read_csv('ml-1m/users.dat', sep='::', header=None, engine='python', encoding='latin-1')
+all_ratings = pd.read_csv('ml-1m/ratings.dat', sep='::', header=None, engine='python', encoding='latin-1')
+
+# prepare the training set and the test set
+training_set = pd.read_csv('ml-100k/u1.base', delimiter='\t')
+training_set = np.array(training_set, dtype='int')
+# prepare the test set and the test set
+test_set = pd.read_csv('ml-100k/u1.test', delimiter='\t')
+test_set = np.array(test_set, dtype='int')
+
 num_users, num_movies = get_num_user_movies(training_set, test_set)
 training_set = convert_bm_matrix(training_set, nb_users=num_users, nb_movies=num_movies)
+test_set = convert_bm_matrix(test_set, nb_users=num_users, nb_movies=num_movies)
+
+training_set = torch.FloatTensor(training_set)
+test_set = torch.FloatTensor(test_set)
+
+training_set = convert_to_binary(training_set)
+test_set = convert_to_binary(test_set)
+
+nv = num_movies
+nh = 100  # tunable
+batch_size = 100  # tunable
+steps = 10
+rbm = RBM(nv, nh)
+
+# Training the RBM
+nb_epoch = 10
+for e in range(1, nb_epoch+1):
+    train_loss = 0.
+    s = 0.
+    for id_user in range(0, num_users - batch_size, batch_size):
+        v0 = training_set[id_user:id_user + batch_size]
+        vk = v0
+        ph0, _ = rbm.sample_h(v0)  # only take the activation probabilities
+        for k in range(steps):  # k steps for the constrasted divergence
+            _, hk = rbm.sample_h(vk)
+            _, vk = rbm.sample_v(hk)
+            vk[v0 < 0] = v0[v0 < 0]  # freeze the training on the none-ratings
+        phk, _ = rbm.sample_h(vk)
+        rbm.train(v0, vk, ph0, phk)
+        train_loss += torch.mean(torch.abs(v0[0 <= v0] - vk[v0 >= 0]))
+        s += 1.
+    print('Epoch ' + str(e) + ': loss=' + str(train_loss / s))
+
+# test the RMB
+test_loss = 0.
+s = 0.
+for id_user in range(num_users):
+    v = training_set[id_user:id_user+1]
+    vt = test_set[id_user:id_user+1]  # the target visible values
+    if (len(vt[vt >= 0]) > 0):  # if the sample contains at least one -1 rating
+        _, h = rbm.sample_h(v)
+        _, v = rbm.sample_v(h)
+        test_loss += torch.mean(torch.abs(vt[0 <= vt] - v[vt >= 0]))
+        s += 1.
+print('Test loss=' + str(test_loss / s))
